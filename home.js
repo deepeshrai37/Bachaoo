df118308c2acf2ba85d771d26d6dfd3886518500
Deepesh Rai <deepeshrai02@gmail.com>
2024-05-27 20:27:21 +0530

    gsap added

---
+++ home.js
@@ -0,0 +1,50 @@
+var tl=gsap.timeline();
+tl.from(".nav h1",{
+  y:-30,
+  opacity:0,
+  duration:1,
+  stagger:0.3
+})
+tl.from(".ri-search-eye-line",{
+  y:-30,
+  opacity:0,
+  duration:1,
+  stagger:0.3
+})
+
+tl.from(".nav-prt2 h5",{
+  y:-30,
+  opacity:0,
+  duration:1,
+  stagger:0.3
+})
+tl.from(".left h1",{
+  x:-30,
+  opacity:0,
+  duration:1,
+  stagger:0.3
+})
+tl.from(".left p",{
+  x:-30,
+  opacity:0,
+  duration:1,
+  stagger:0.3
+})
+tl.from(".left button",{
+  y:30,
+  opacity:0,
+  duration:1,
+  stagger:0.3
+})
+tl.from(".center,.content>img",{
+  y:-60,
+  opacity:0,
+  duration:1,
+  stagger:0.3
+})
+tl.from(".right",{
+  x:30,
+  opacity:0,
+  duration:1,
+  stagger:0.3
+})
